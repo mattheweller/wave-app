adb7bf7e43f277ab8044155e91378086a6469df2
mattheweller <mattcjeller@gmail.com>
2021-12-10 15:25:18 +0000

    Add WavePortal.sol

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("I an a contract, smart and short. Here's my constructor, here's my import");
+    }
+}
